d63b8c0518c1e1d3bfde57c8ed0859d8829c6a22
MohamadAlGhorani <moh.gh266@gmail.com>
2020-03-21 20:38:49 +0100

    add license

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -5,46 +5,46 @@ require("dotenv").config();
 const dataStapEen = [];
 const dataStapTwee = [];
 
-router.get("/", function (req, res) {
+router.get("/", function(req, res) {
+  res.render("stapEen", {
+    title: "Inloggen"
+  });
+});
+
+router.get("/stap-twee", function(req, res) {
+  console.log(req.query);
+  if (req.query.studentNummer) {
+    dataStapEen.push(req.query);
+  } else {
+    dataStapTwee.push(req.query);
+  }
+  if (req.query.user || req.query.studentNummer) {
+    console.log("user", dataStapEen);
+    console.log("info", dataStapTwee);
+    let infoData = dataStapTwee.filter(student => {
+      if (req.query.studentNummer) {
+        return student.user == req.query.studentNummer;
+      } else {
+        return student.user == req.query.user;
+      }
+    });
+    console.log("daataaa", infoData);
+    res.render("stapTwee", {
+      title: "enquete invullen",
+      data: infoData,
+      user: req.query.studentNummer
+    });
+  } else {
     res.render("stapEen", {
-        title: "Inloggen",
+      title: "Inloggen"
     });
+  }
 });
 
-router.get("/stap-twee", function (req, res) {
-    console.log(req.query);
-    if (req.query.studentNummer) {
-        dataStapEen.push(req.query);
-    } else {
-        dataStapTwee.push(req.query);
-    }
-    if (req.query.user || req.query.studentNummer) {
-        console.log("user", dataStapEen);
-        console.log("info", dataStapTwee);
-        let infoData = dataStapTwee.filter(student => {
-            if (req.query.studentNummer) {
-                return student.user == req.query.studentNummer;
-            } else {
-                return student.user == req.query.user;
-            }
-        });
-        console.log("daataaa", infoData);
-        res.render("stapTwee", {
-            title: "enquete invullen",
-            data: infoData,
-            user: req.query.studentNummer
-        });
-    } else {
-        res.render("stapEen", {
-            title: "Inloggen",
-        });
-    }
-});
-
-router.get("/stap-dree", function (req, res) {
-    res.render("stapDree", {
-        title: "Thanx"
-    });
+router.get("/stap-dree", function(req, res) {
+  res.render("stapDree", {
+    title: "Thanx"
+  });
 });
 
 module.exports = router;
